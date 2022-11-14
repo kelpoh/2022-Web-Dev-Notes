02bc64a006d865d560efa59ead5cd557b314456e
Eleni <eloonku@gmail.com>
2022-11-14 14:57:50 -0500

    cart server now working

--- server/controllers/cart.js
+++ server/controllers/cart.js
@@ -1,5 +1,7 @@
+//@ts-check
+
 const express = require('express');
-const { get, add, remove } = require('../models/cart');
+const { get, add, update } = require('../models/cart');
 
 const app = express.Router();
 
@@ -9,11 +11,13 @@ app.get('/:userId', (req, res) => {
 });
 
 app.post('/:userId/:productId/:quantity', (req, res) => {
-    res.send(add(req.params.productId, req.params.userId, req.params.quantity));
+    res.send(add(req.params.userId, +req.params.productId, +req.params.quantity));
 });
 
-app.delete('/:userId/:productId', (req, res) => {
-    res.send(remove(req.params.userId, req.params.productId));
-});
+// the plus indicates a number in JavaScript
+
+app.patch('/:userId/:productId/:quantity', (req, res) => {
+    res.send(update(req.params.userId, +req.params.productId, +req.params.quantity));
+}); // patch means detail changes, put replaces
 
 module.exports = app;
--- server/models/cart.js
+++ server/models/cart.js
@@ -2,27 +2,53 @@
 // CartItem: { id: 1, quantity: 2, productId: 1, userId: 'mp@np.edu' }
 const list = [];
 
+const get = (userId) => {
+  return list.filter((cartItem) => cartItem.userId === userId);
+  };
+
 ;
-const add = (productId, userId, quantity) => {
+
+/**
+ * 
+ * @param {string} userId 
+ * @param {number} productId 
+ * @param {number} quantity 
+ * @returns 
+ */
+
+const add = (userId, productId, quantity) => {
     let cartItem = list.find((cartItem) => cartItem.userId === userId && cartItem.productId === productId);
     if (cartItem) {
-        cartItem.quantity += +quantity;
+        cartItem.quantity += quantity;
     } else {
-        list.push({ id: list.length + 1, quantity: +quantity, productId, userId });
+      cartItem = { id: list.length + 1, quantity, productId, userId }
+        list.push(cartItem);
     }
     return cartItem;
 };
 
-const get = (userId) => {
-    return list.filter((cartItem) => cartItem.userId === userId);
-    };
+/**
+ * 
+ * @param {string} userId 
+ * @param {number} productId 
+ * @param {number} quantity 
+ * @returns 
+ */
 
-const remove = (userId, productId) => {
+const update = (userId, productId, quantity) => {
   const index = list.findIndex((cartItem) => cartItem.userId === userId && cartItem.productId === productId);
   if (index !== -1) {
-    list.splice(index, 1);
+    if (quantity === 0) {
+      list.splice(index, 1);
+    }
+    else {
+      list[index].quantity = quantity;
+    }
+  }
+  else {
+     throw new Error('Cart item not found.');
   }
   return index;
 }
 
-module.exports = { add, get, remove }
+module.exports = { add, get, update }
