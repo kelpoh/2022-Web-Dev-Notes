18df19a8f46b363151b3ecb68537788465710ace
Eleni <eloonku@gmail.com>
2022-12-01 14:22:50 -0500

    mongodb changes

--- server/models/mongo.js
+++ server/models/mongo.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = process.env.MONGO_URI ?? "";
+const uri = process.env.MONGO_URI;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 module.exports = {
-    connect: ()=> client.connect() 
+    connect: ()=> client.connect()
 }
--- server/models/products.js
+++ server/models/products.js
@@ -1,20 +1,21 @@
 const data = require('../data/products.json');
+
 const { connect } = require('./mongo');
 
-async function collection(){
+async function collection() {
     const client = await connect();
-    return client.db('chopiphy').collection('products');
+    return client.db("Shoperator").collection("products");
 }
 
 async function getProducts() {
     const db = await collection();
-    const data = await db.find().toArray()
+    const data = await collection().find().toArray()
     return data;
 }
 
 async function getProduct(id) {
     const db = await collection();
-    const data = await db.findOne({ _id: id })
+    const data = await collection().findOne({ _id: id})
     return data;
 }
 
